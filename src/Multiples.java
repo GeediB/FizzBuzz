25eff48850995d6acd98e077b3ba8b1a80bbe3f5
GeediB <geedi.bryden@mail.utoronto.ca>
2024-09-11 19:46:27 -0400

    -m "Reduce method"

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,7 +2,7 @@ public class Multiples {
     public static void main(String[] args) {
 
         int i ;
-        for (i = 0; i <= 1000; ) {
+        for (i = 0; i <= 1000;){
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
             if (divisibleBy3|| !divisibleBy5) {
---
+++ src/Reduce.java
@@ -0,0 +1,20 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int n = 100;
+
+        while (n >= 0) {
+            boolean even = n % 2 == 0;
+            if (even) {
+                n = n / 2;
+            }
+            else{
+                n--;
+            }
+            i++;
+        }
+        System.out.println(i);
+    }
+}
+
